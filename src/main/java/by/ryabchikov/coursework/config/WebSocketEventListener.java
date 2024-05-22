09d612088f4313ef5901bc61a90df52de1314d58
Nikita Ryabchikov <nryabchikov24@gmail.com>
2024-05-22 11:18:05 +0300

    feat: chat configuration

---
+++ src/main/java/by/ryabchikov/coursework/config/WebSocketEventListener.java
@@ -0,0 +1,32 @@
+package by.ryabchikov.coursework.config;
+
+import by.ryabchikov.coursework.model.user.ChatMessage;
+import by.ryabchikov.coursework.util.MessageType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+    private final SimpMessageSendingOperations sendingOperations;
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null) {
+            log.info("User disconnected: {}", username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            sendingOperations.convertAndSend("/topic/public", chatMessage);
+        }
+    }
+}
